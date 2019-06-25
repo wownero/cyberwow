6f79a8b010727b73ec97f16af4cf887f65c1fc5f
fuwa <fuwa25519@protonmail.com>
2019-06-25 15:12:01 +0800

    add targetHeight helper

--- cyberwow/lib/controller/helper.dart
+++ cyberwow/lib/controller/helper.dart
@@ -21,8 +21,11 @@ along with CyberWOW.  If not, see <https://www.gnu.org/licenses/>.
 
 import 'dart:async';
 import 'dart:io';
+import 'dart:convert';
 
 import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
 
 Future<String> getBinaryPath(String name) async {
   final tmpDir = await getTemporaryDirectory();
@@ -32,4 +35,38 @@ Future<String> getBinaryPath(String name) async {
 Future<bool> binaryExists(String name) async {
   final binPath = await getBinaryPath(name);
   return new File(binPath).exists();
+}
+
+Future<int> targetHeight() async {
+  var url = '';
+  if (kReleaseMode) {
+    url = 'http://127.0.0.1:34568/json_rpc';
+  } else {
+    url = 'http://192.168.10.100:34568/json_rpc';
+  }
+
+  final body = json.encode
+  (
+    {
+      'jsonrpc': '2.0',
+      'id': '0',
+      'method': 'sync_info',
+    }
+  );
+
+  var response = await http.post
+  ( url,
+    body: body
+  );
+
+  print('Response status: ${response.statusCode}');
+  if (response.statusCode != 200) {
+    return -1;
+  } else {
+    final responseBody = json.decode(response.body)['result'];
+    final targetHeight = responseBody["target_height"];
+    // print('height: ${responseBody["height"]}');
+
+    return targetHeight;
+  }
 }
