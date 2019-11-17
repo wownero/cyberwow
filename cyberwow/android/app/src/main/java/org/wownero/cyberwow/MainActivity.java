ab958d95e19a88781bafb7bdb083ae93100ff051
fuwa <fuwa25519@protonmail.com>
2019-11-17 05:22:47 +0000

    fix navigation color on start up

--- cyberwow/android/app/src/main/java/org/wownero/cyberwow/MainActivity.java
+++ cyberwow/android/app/src/main/java/org/wownero/cyberwow/MainActivity.java
@@ -1,6 +1,7 @@
 package org.wownero.cyberwow;
 
 import android.os.Bundle;
+import android.graphics.Color;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -8,6 +9,7 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    getWindow().setNavigationBarColor(Color.BLACK);
     GeneratedPluginRegistrant.registerWith(this);
   }
 }
